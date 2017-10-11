36f3c43c4fc1ea29feb9f0f6f3cc66a5f728d8b4
MarisaMiller <marisa.e.miller@gmail.com>
2017-10-11 15:13:15 -0500

    fixed location of MSI job script

--- analysis/qc_mapping_pipelineMSI.sh
+++ analysis/qc_mapping_pipelineMSI.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#PBS -l nodes=1:ppn=24,walltime=48:00:00
+#PBS -l nodes=1:ppn=24,walltime=24:00:00
 #PBS -m abe
 #PBS -M millerme@umn.edu
 #PBS -N SNP_pipeline
@@ -25,8 +25,8 @@ done
 #Remove duplicate values in list to just get unique basenames
 uniq=($(printf "%s\n" "${filelist2[@]}" | uniq)); echo "${uniq[@]}"
 
-#Then, unse the unique array above in shell script w/ SNP pipeline to submit to MSI queues
+#Then, use the unique array above in shell script w/ SNP pipeline to submit to MSI queues
 for F in "${uniq[@]}"
 do
-bash SNP_pipeline.sh -n ../../raw_reads/$F -g ../../reference_genomes/Puccinia_coronata_avenae_12SD80.primary.fa
+bash ../scripts/QC_mapping_pipeline.sh -n ../raw_reads/$F -g ../reference_genomes/Puccinia_coronata_avenae_12SD80.primary.fa
 done
